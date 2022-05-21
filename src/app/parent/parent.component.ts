57a08776b02a4f089e40244bfe78971caad8ca90
shan <shanmugamanimeyialagan@gmail.com>
2022-05-21 22:13:29 +0530

    directives

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Sample } from '../sample';
 @Component({
   selector: 'app-parent',
   templateUrl: './parent.component.html',
@@ -12,11 +12,21 @@ export class ParentComponent implements OnInit {
   ngOnInit(): void {
   }
    name:string='shanmugamani';
-   fromchild:any="parent works!";
+   fromchild:any="parent";
+
+
+   students:Sample[]=[
+     {regno:12,name:'shan'},
+     {regno:13,name:'krish'},
+     {regno:14,name:'murali'},
+     {regno:15,name:'sibin'},
+     {regno:16,name:'bill'},
+
+    ]
 
 
    receivedata(event:any){
-    this.fromchild=event;
+    this.name=event;
 
    }
 
