2d024bdd17eee63889fbfafabdd664ce9b4902eb
shan <shanmugamanimeyialagan@gmail.com>
2022-05-24 17:46:34 +0530

    host listner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef,HostBinding,HostListener} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -14,4 +14,13 @@ export class AppComponent {
 
     
   }
+  
+  // host binding method used to bind properties for host or component
+  @HostBinding ('style.color') color='blue';
+  // we can set the variable for this binding,so we can change dynamically
+
+  // @host listner used to how to handle user events
+
+
+  
 }
